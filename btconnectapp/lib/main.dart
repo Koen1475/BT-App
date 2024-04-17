666655d857cf12a50530910f1a7aece9809e6a5e
Koen Hilbrands <I519462@fhict.nl519462@student.fontys.nl>
2024-04-17 11:45:59 +0200

    added screen to send files

--- btconnectapp/lib/main.dart
+++ btconnectapp/lib/main.dart
@@ -1,24 +1,28 @@
 import 'package:all_bluetooth/all_bluetooth.dart';
 import 'package:flutter/material.dart';
+
 import 'screens/chat_screen.dart';
 import 'screens/home_screen.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
 final allBluetooth = AllBluetooth();
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: StreamBuilder<Object>(
+      home: StreamBuilder(
           stream: allBluetooth.listenForConnection,
           builder: (context, snapshot) {
             final result = snapshot.data;
+            if (result?.state == true) {
+              return const ChatScreen();
+            }
             print(result);
             return const HomeScreen();
           }),
